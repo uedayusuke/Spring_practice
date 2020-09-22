c44c75b2b7f15627f98034d52c9c15dd5d646cb9
uedayusuke <uedayusuke1107@gmail.com>
2020-09-22 18:35:56 +0900

    [ADD]フォームクラスを追加

--- NoDI/src/main/java/com/example/demo/ShainForm.java
+++ NoDI/src/main/java/com/example/demo/ShainForm.java
@@ -4,13 +4,18 @@ import javax.validation.constraints.NotEmpty;
 
 public class ShainForm {
 
+	// "@NotEmpty"で空出ないかをチェックする。
+	// →もし空だったらmessageを表示
 	@NotEmpty(message = " 社員Noを入れてください")
+	// index.htmlの"number(引数)"を定義
 	private String number;
 
+	// numberを取得する
 	public String getNumber() {
 		return number;
 	}
 
+	// numberを設置する
 	public void setNumber(String number) {
 		this.number = number;
 	}
