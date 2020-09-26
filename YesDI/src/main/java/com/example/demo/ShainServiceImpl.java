f78a90f879a3e3ecc0bfa7aaa3f999a67709086b
uedayusuke <uedayusuke1107@gmail.com>
2020-09-26 19:53:43 +0900

    [ADD]serviceをBean化

---
+++ YesDI/src/main/java/com/example/demo/ShainServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+
+// "@Service"でserviceを宣言
+@Service
+public class ShainServiceImpl implements ShainService {
+
+	private final ShainRepository shainRepository;
+
+	//ShainRepositoryをDI化
+	public ShainServiceImpl(ShainRepository shainRepository) {
+		this.shainRepository = shainRepository;
+	}
+
+	@Override
+	public String findByNo(String number) {
+		//リポジトリから社員を選択
+		String name = shainRepository.selectByNo(number);
+		return name;
+	}
+
+}
