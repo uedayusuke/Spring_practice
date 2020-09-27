ef86930862b8e61affe8579f4de00f80326159dd
uedayusuke <uedayusuke1107@gmail.com>
2020-09-27 17:19:32 +0900

    [FIX]validationを修正

--- NoDI/src/main/java/com/example/demo/ShainForm.java
+++ NoDI/src/main/java/com/example/demo/ShainForm.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public class ShainForm {
 
 	// "@NotEmpty"で空ではないかをチェックする。
 	// →もし空だったらmessageを表示
-	@NotNull(message = " 社員Noを入れてください")
+	@NotEmpty(message = " 社員Noを入れてください")
 	
 	// 下記のアノテーションで半角数字3桁のvalidationを設定
 	@Pattern(regexp = "\\d{3}", message = "半角数字、3ケタでお願いいたします。")
