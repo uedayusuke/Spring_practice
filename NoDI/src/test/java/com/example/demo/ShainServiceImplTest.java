c085c847b800a7bc11e47fa75b2d84e64101d6c4
uedayusuke <uedayusuke1107@gmail.com>
2020-09-27 12:47:39 +0900

    [ADD]ShainServiceImplTest

--- NoDI/src/test/java/com/example/demo/ShainServiceImplTest.java
+++ NoDI/src/test/java/com/example/demo/ShainServiceImplTest.java
@@ -8,15 +8,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+// "@RunWith"でJUnitテストを行う宣言
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ShainServiceImplTest {
 
+	// "@MockBean"で見せかけのBeanを定義→"shainRepository"をMock化
 	@MockBean
 	private ShainRepository shainRepository;
 
 	@Test
 	public void findByNoTest() throws Exception {
-		//shainRepositoryをモックにして、findByNoが機能するか？のテスト
+		// shainRepositoryをモックにして、findByNoが機能するか？のテスト
+		// shainRepositoryのselectByNoに"100"が入った時、"佐藤"が帰ってくるように定義
 		when(shainRepository.selectByNo("100")).thenReturn("佐藤");
 		ShainService shainService = new ShainServiceImpl();
 		String employee = shainService.findByNo("100");
